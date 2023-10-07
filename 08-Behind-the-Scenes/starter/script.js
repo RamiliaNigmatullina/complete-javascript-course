f80ca05c66c23c32f03ccd85d65d356ff802e737
Ramilia Nigmatullina <RamiliaNigmatullina@gmail.com>
2023-10-07 16:58:25 +0200

    add lesson 100 primitives vs objects in practice

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -151,7 +151,7 @@
 // matilda.calcAge();
 
 // // 6
-// const f = jonas.calcAge; // coping
+// const f = jonas.calcAge; // copying
 // f(); // Uncaught TypeError: Cannot read property 'year' of undefined
 
 //
@@ -228,3 +228,42 @@
 // };
 
 // jonas2.calcAge();
+
+//
+//
+//
+// 100. Primitives vs. objects in practice
+
+// Primitive types
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+console.log(lastName, oldLastName);
+
+// Reference types
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davis';
+console.log(jessica.lastName, marriedJessica.lastName);
+// marriedJessica = {} // Uncaught TypeError: Assignment to constant variable
+
+// Copying objects
+const jessica2 = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+  family: ['Alice', 'Bob'],
+};
+
+const jessicaCopy = Object.assign({}, jessica2); // shallow copy
+jessicaCopy.lastName = 'Davis';
+
+jessicaCopy.push('Mary');
+jessicaCopy.push('John');
+
+console.log('Before marriage', jessica2);
+console.log('After marriage', jessicaCopy);
