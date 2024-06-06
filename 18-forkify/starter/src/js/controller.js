70ee0b580ba1c30f88865a03312d5f08bf6c0ab6
RamiliaNigmatullina <ramilianigmatullina@gmail.com>
2024-06-06 21:05:12 +0200

    add lesson 295. Event Handlers in MVC: Publisher-Subscriber Pattern

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -27,12 +27,11 @@ const controlRecipes = async function () {
     // 2) Rendering receipe
     recipeView.render(model.state.recipe);
   } catch (err) {
-    alert(err);
+    console.log(err);
   }
 };
 
-['hashchange', 'load'].forEach(ev =>
-  window.addEventListener(ev, controlRecipes)
-);
-// window.addEventListener('hashchange', controlRecipes);
-// window.addEventListener('load', controlRecipes);
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+init();
--- 18-forkify/starter/src/js/helpers.js
+++ 18-forkify/starter/src/js/helpers.js
@@ -1,4 +1,4 @@
-import { TIMEOUT_SEC } from './helpers.js';
+import { TIMEOUT_SEC } from './config.js';
 
 export const timeout = function (s) {
   return new Promise(function (_, reject) {
