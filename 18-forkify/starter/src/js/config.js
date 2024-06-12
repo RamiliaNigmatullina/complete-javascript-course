b2da9886274ef474e3f3d138bd7531783e336c52
RamiliaNigmatullina <ramilianigmatullina@gmail.com>
2024-06-12 20:23:51 +0200

    add lesson 309. Uploading a New Recipe - Part 2

--- 18-forkify/starter/src/js/config.js
+++ 18-forkify/starter/src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = '6f7f7e9c-2b48-461a-b119-1eb91482faf9';
+export const MODAL_CLOSE_SEC = 2.5;
